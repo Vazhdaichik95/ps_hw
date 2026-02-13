ad4e15ca8291c1177343bc07257188ad7dd3008b
Vazhdaichik95 <vazhdaichik95@yandex.ru>
2026-02-13 23:27:15 +0300

    2-dice

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 'use strict';
+function getRandomFromDice(dice) {
+  const diceWithoutD = String(dice).replace('d','');
+  if (Number.isFinite(diceWithoutD)) return null;
+
+  const numDice = Number.parseInt(diceWithoutD, 10);
+
+  if(numDice<4 || numDice>20) return null;
+
+  return Math.floor(Math.random()*(numDice-1+1)+1);
+}
