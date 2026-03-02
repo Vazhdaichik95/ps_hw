de13419a9377ae7e9005ca71bb0a1773d1a96b4a
Vazhdaichik95 <vazhdaichik95@yandex.ru>
2026-03-02 23:14:20 +0300

    8-solid

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 'use strict';
+class Billing {
+  amount;
+
+  constructor(amount) {
+    this.amount = amount;
+  }
+
+  totalAmount() {
+    return this.amount;
+  }
+}
+
+class FixBilling extends Billing{
+  constructor(amount) {
+    super(amount);
+  }
+}
+
+class HourBilling extends Billing {
+  #hours;
+
+  constructor(amount, hours) {
+    super(amount);
+    this.#hours = hours;
+  }
+
+  totalAmount() {
+    return this.#hours*this.amount;
+  }
+}
+
+class ItemBilling extends Billing {
+  #items;
+
+  constructor(amount, items) {
+    super(amount);
+    this.#items = items;
+  }
+
+  totalAmount() {
+    return this.#items*this.amount;
+  }
+}
